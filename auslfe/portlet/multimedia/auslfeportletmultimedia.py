5a50a713970c970af6fc9a232341d86c2b9cafd4
federicadelia <federica.delia@redturtle.it>
2010-04-22 08:27:12 +0000

    portlet multimedia

--- auslfe/portlet/multimedia/auslfeportletmultimedia.py
+++ auslfe/portlet/multimedia/auslfeportletmultimedia.py
@@ -11,39 +11,13 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class IAuslfePortletMultimedia(IPortletDataProvider):
-    """A portlet
-
-    It inherits from IPortletDataProvider because for this portlet, the
-    data that is being rendered and the portlet assignment itself are the
-    same.
-    """
-
-    # TODO: Add any zope.schema fields here to capture portlet configuration
-    # information. Alternatively, if there are no settings, leave this as an
-    # empty interface - see also notes around the add form and edit form
-    # below.
-
-    # some_field = schema.TextLine(title=_(u"Some field"),
-    #                              description=_(u"A field to use"),
-    #                              required=True)
-
+    """A portlet"""
+    
 
 class Assignment(base.Assignment):
-    """Portlet assignment.
-
-    This is what is actually managed through the portlets UI and associated
-    with columns.
-    """
+    """Portlet assignment"""
 
     implements(IAuslfePortletMultimedia)
-
-    # TODO: Set default values for the configurable parameters here
-
-    # some_field = u""
-
-    # TODO: Add keyword parameters for configurable parameters here
-    # def __init__(self, some_field=u""):
-    #    self.some_field = some_field
 
     def __init__(self):
         pass
@@ -53,52 +27,23 @@ class Assignment(base.Assignment):
         """This property is used to give the title of the portlet in the
         "manage portlets" screen.
         """
-        return "AUuslfe Portlet Multimedia"
+        return "Auslfe Portlet Multimedia"
 
 
 class Renderer(base.Renderer):
-    """Portlet renderer.
-
-    This is registered in configure.zcml. The referenced page template is
-    rendered, and the implicit variable 'view' will refer to an instance
-    of this class. Other methods can be added and referenced in the template.
-    """
+    """Portlet renderer"""
 
     render = ViewPageTemplateFile('auslfeportletmultimedia.pt')
 
 
 class AddForm(base.AddForm):
-    """Portlet add form.
-
-    This is registered in configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display. The create() method actually
-    constructs the assignment that is being added.
-    """
+    """Portlet add form"""
     form_fields = form.Fields(IAuslfePortletMultimedia)
 
     def create(self, data):
         return Assignment(**data)
 
 
-# NOTE: If this portlet does not have any configurable parameters, you
-# can use the next AddForm implementation instead of the previous.
-
-# class AddForm(base.NullAddForm):
-#     """Portlet add form.
-#     """
-#     def create(self):
-#         return Assignment()
-
-
-# NOTE: If this portlet does not have any configurable parameters, you
-# can remove the EditForm class definition and delete the editview
-# attribute from the <plone:portlet /> registration in configure.zcml
-
-
 class EditForm(base.EditForm):
-    """Portlet edit form.
-
-    This is registered with configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display.
-    """
+    """Portlet edit form"""
     form_fields = form.Fields(IAuslfePortletMultimedia)
