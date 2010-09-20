7d394b722cd1851833079afd3de0a73ee2f519ec
keul <luca@keul.it>
2010-09-20 21:17:34 +0000

    Template fixes, for Plone 4.0

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='auslfe.portlet.multimedia',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Plone',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
