3aad698dbdd502f4261601cb7f453699a5324192
Luca Fabbri <luca@keul.it>
2014-10-29 15:10:55 +0100

    fixed migration stuff

--- auslfe/portlet/multimedia/browser/auslfe-multimediaportlet.js
+++ auslfe/portlet/multimedia/browser/auslfe-multimediaportlet.js
@@ -16,96 +16,101 @@ jQuery.auslfe_multimedia = {
 	}
 };
 
-jq(document).ready(function() {
-	
-	// Site language
-	var lang = jq("html").attr('lang') || 'en';
-	var i18n = jq.auslfe_multimedia.i18n[lang] || jq.auslfe_multimedia.i18n['en'];
-	
-	/**
-	 * A function for random sorting an array or aray-like object
-	 */
-	function randOrd(){
-		return (Math.round(Math.random())-0.5);
-	} 
-	
-	/**
-	 * At load time, remove all images and stop links HREF
-	 */
-	jq('.portletMultimedia .galleryMultimedia a').each(function() {
-		jq(this).attr("href", "javascript:;").find("img").remove();
-	});
-	
-	/**
-	 * Prepare portlets to AJAX load images from server
-	 */
-	jq('.portletMultimedia').each(function(index) {
-		var portlet = jq(this);
-		// Load random images?
-		var rnd = jq("span.random",portlet).length>0;
-		// Client reload images?
-		var client_reload = jq("span.client_reload",portlet).length>0;
-		var link = jq(".portletFooter a", portlet);
-		// var timestamps = new Date().getTime();
-		var images = null;
+(function($) {
+
+	$(document).ready(function() {
+		
+		// Site language
+		var lang = $("html").attr('lang') || 'en';
+		var i18n = $.auslfe_multimedia.i18n[lang] || $.auslfe_multimedia.i18n['en'];
 		
 		/**
-		 * Change order on the stored images
-		 * @param {boolean} startHidden true if images must start hidden
+		 * A function for random sorting an array or aray-like object
 		 */
-		function reorder(startHidden) {
-			var startHidden = startHidden || false;
-			if (rnd) images.sort(randOrd);
-			jq(".galleryMultimedia a", portlet).each(function(index) {
-				var link = jq(this);
-				var curData = images[index];
-				if (!curData.image)
-					curData.image = jq('<img alt="'+curData.description+'" title="'+curData.title+'" src="'+curData.url+'/image_tile" '+(startHidden?' style="display:none"':'')+'/>');
-				link.append(curData.image);
-				curData.image.imagesLoaded(function(e) {
-					jq(this).fadeIn("fast");
-				});
-				link.attr("href", curData.url+"/image_view_fullscreen");
-			});
-		}
+		function randOrd(){
+			return (Math.round(Math.random())-0.5);
+		} 
 		
-		jq.get(link.attr('href')+'/@@query_images', {}, function(data) {
-			images = data;
-			reorder();
-			portlet.removeClass("hideFlag");
-		}, dataType='json');
+		/**
+		 * At load time, remove all images and stop links HREF
+		 */
+		$('.portletMultimedia .galleryMultimedia a').each(function() {
+			$(this).attr("href", "javascript:;").find("img").remove();
+		});
 		
-		if (rnd && client_reload) {
-			// 1 - bind the reload image event
-			portlet.bind("portletRefresh", function(event) {
-				jq("img", portlet).fadeOut("fast", function() {
-					jq("img", portlet).remove();
-					reorder(true);
-				});
-			});
+		/**
+		 * Prepare portlets to AJAX load images from server
+		 */
+		$('.portletMultimedia').each(function(index) {
+			var portlet = $(this);
+			// Load random images?
+			var rnd = $("span.random",portlet).length>0;
+			// Client reload images?
+			var client_reload = $("span.client_reload",portlet).length>0;
+			var link = $(".portletFooter a", portlet);
+			// var timestamps = new Date().getTime();
+			var images = null;
 			
-			var reloadEventHandler = function() {
-				portlet.trigger("portletRefresh");
-			};
-			var reload_timeout = this.getAttribute('data-reloadtimeout') || jq.auslfe_multimedia.timeout;
-			var intval = setInterval(reloadEventHandler, reload_timeout);
-
-			// 2 - handle clicks on portlet title
-			jq(".portletHeader", this).attr('title', i18n['stopReload']);
-			jq(".portletHeader", this).click(function(e) {
-				client_reload = !client_reload;
-				if (client_reload) {
-					jq(this).attr('title', i18n['stopReload']);
-					intval = setInterval(reloadEventHandler, reload_timeout);
-					reloadEventHandler();
-				}
-				else {
-					jq(this).attr('title', i18n['restartReload']);
-					clearInterval(intval);
-				}
-			});
-		}
-		
+			/**
+			 * Change order on the stored images
+			 * @param {boolean} startHidden true if images must start hidden
+			 */
+			function reorder(startHidden) {
+				var startHidden = startHidden || false;
+				if (rnd) images.sort(randOrd);
+				$(".galleryMultimedia a", portlet).each(function(index) {
+					var link = $(this);
+					var curData = images[index];
+					if (!curData.image)
+						curData.image = $('<img alt="'+curData.description+'" title="'+curData.title+'" src="'+curData.url+'/image_tile" '+(startHidden?' style="display:none"':'')+'/>');
+					link.append(curData.image);
+					curData.image.imagesLoaded(function(e) {
+						$(this).fadeIn("fast");
+					});
+					link.attr("href", curData.url+"/image_view_fullscreen");
+				});
+			}
+			
+			$.get(link.attr('href')+'/@@query_images', {}, function(data) {
+				images = data;
+				reorder();
+				portlet.removeClass("hideFlag");
+			}, dataType='json');
+			
+			if (rnd && client_reload) {
+				// 1 - bind the reload image event
+				portlet.bind("portletRefresh", function(event) {
+					$("img", portlet).fadeOut("fast", function() {
+						$("img", portlet).remove();
+						reorder(true);
+					});
+				});
+				
+				var reloadEventHandler = function() {
+					portlet.trigger("portletRefresh");
+				};
+				var reload_timeout = this.getAttribute('data-reloadtimeout') || $.auslfe_multimedia.timeout;
+				var intval = setInterval(reloadEventHandler, reload_timeout);
+	
+				// 2 - handle clicks on portlet title
+				$(".portletHeader", this).attr('title', i18n['stopReload']);
+				$(".portletHeader", this).click(function(e) {
+					client_reload = !client_reload;
+					if (client_reload) {
+						$(this).attr('title', i18n['stopReload']);
+						intval = setInterval(reloadEventHandler, reload_timeout);
+						reloadEventHandler();
+					}
+					else {
+						$(this).attr('title', i18n['restartReload']);
+						clearInterval(intval);
+					}
+				});
+			}
+			
+		});
 	});
-});
+	
+})(jQuery);
+
 
